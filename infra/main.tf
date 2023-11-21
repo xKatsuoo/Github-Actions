73f3d22f3017b0ad3e8e0060d23e65b9cb2b2e5c
xKatsuoo <jasonkrimmel@songoohan.de>
2023-11-21 13:36:38 +0100

    changed: cicd yml

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,4 @@
 module "s3-website" {
   source = "./modules/s3-website"
-  bucket_name = "github-bucket-543fds"
+  bucket_name = "github-bucket-214fds"
 }
--- infra/modules/s3-website/outputs.tf
+++ infra/modules/s3-website/outputs.tf
@@ -1,3 +1,3 @@
-output "bucket_arn" {
-  value = aws_s3_bucket.this.arn
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
 }
